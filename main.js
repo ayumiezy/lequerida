60a56887150821eaa5a967b6da234f3b9d2e1b29
ayumi <105938742+ayumiezy@users.noreply.github.com>
2024-03-26 20:24:01 -0300

    g

--- main.js
+++ main.js
@@ -20,8 +20,12 @@ const tempoObjetivo1 = new Date("2023-10-05T00:00:00");
 contadores[0].textContent = calculaTempo(tempoObjetivo1);
 
 function calculaTempo(tempoObjetivo) {
-  let tempoAtual = new Date();
-  let tempoFinal = tempoObjetivo - tempoAtual;
-  let segundos = tempoFinal / 1000;
-  return segundos;
-}
+    let tempoAtual = new Date();
+    let tempoFinal = tempoObjetivo - tempoAtual;
+    let segundos = Math.floor(tempoFinal / 1000);
+    let minutos = Math.floor(segundos / 60);
+    let horas = Math.floor(minutos / 60);
+    let dias = Math.floor(horas / 24);
+    return dias;
+  }
+  
